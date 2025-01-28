057d9566553be2d68a272d14a657bed4cb2fcce9
Adarsh-Nagar-Ops <adarshnagar247@gmail.com>
2025-01-29 00:29:25 +0530

    Autoplay and add Event Listener

--- RPS.js
+++ RPS.js
@@ -3,6 +3,23 @@ let score = JSON.parse(localStorage.getItem('scores'))
 function updateScoreDisplay() {
     document.querySelector('.score').innerHTML = `Wins: ${score.wins}; Ties: ${score.ties}; Losses: ${score.loses}`;
 }
+document.querySelector('.Rock').addEventListener('click',()=>{
+    checkWhoWin('Rock');
+});
+document.querySelector('.Paper').addEventListener('click',()=>{
+    checkWhoWin('Paper');
+});
+document.querySelector('.Scissors').addEventListener('click',()=>{
+    checkWhoWin('Scissors');
+});
+document.body.addEventListener('keydown',(event)=>{
+    if(event.key=== 'r')
+        checkWhoWin('Rock');
+    else if(event.key=== 'p')
+        checkWhoWin('Paper');
+    else if(event.key=== 's')
+        checkWhoWin('Scissors');
+});
 function genCompMove() {
     let computerMove;
     let rand = Math.floor(Math.random() * (3)) + 1;
@@ -57,7 +74,7 @@ function Autoplay() {
     const button = document.querySelector('.AutoPlay'); // Updated selector
     if (!isAutoPlay) {
         button.innerHTML = 'Stop Play'; // Correct label for stopping autoplay
-        interval = setInterval(function() {
+        interval = setInterval(()=> {
             checkWhoWin(genCompMove());
         }, 1000);
         isAutoPlay = true;
